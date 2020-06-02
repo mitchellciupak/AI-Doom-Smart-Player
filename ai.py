f20dd0cb2adeba8c480ad0fcffef9825cdf61396
mitchellciupak <ciupak@purdue.edu>
2020-06-02 14:58:10 -0500

    CNN Constructor Def

--- ai.py
+++ ai.py
@@ -28,8 +28,8 @@ class CNN(nn.Module):
     
     def _init_(self, number_actions):
         super(CNN, self).__init__()
-        self.convolution1
-        self.convolution2
-        self.convolution3
-        self.fc1
-        self.fc2
+        self.convolution1 = nn.Conv2d(in_channels = 1, out_channels = 32, kernel_size = 5) # 1 Black and White image, 32 detected features, 5x5 feature detector 
+        self.convolution2 = nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = 3) # In channels are now out channels of last convolution connection 
+        self.convolution3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 2)
+        self.fc1 = nn.Linear(in_features = number_neurons, out_features = 40)
+        self.fc2 = nn.Linear(in_features = 40, out_features = numver_actions)
